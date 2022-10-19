564a6c4ec0854b7d1e398dce4c5dafa99c226a30
meisReal <simas.cesnakauskas@gmail.com>
2022-10-19 21:09:33 +0300

    server.js file with basic server function to give response

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 const http = require('http');
+const products = require('./data/products')
+
+const server = http.createServer((req, res) => {
+    // MOST BASIC RES
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<h1>Hello World</h1>');
+    // res.end();
+
+    // NORMAL
+    if (req.url === '/api/products') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(products));
+        res.end();
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: 'Route not found' }));
+        res.end();
+    }
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
