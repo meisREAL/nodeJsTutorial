b45cd4207cd5dc415a90418f0c97494a42554ef0
meisReal <simas.cesnakauskas@gmail.com>
2022-10-21 11:21:09 +0300

    getPostData moved to util

--- utils.js
+++ utils.js
@@ -1,4 +1,6 @@
+const { rejects } = require('assert');
 const fs = require('fs');
+const { resolve } = require('path');
 
 function writeDataToFile(filename, content) {
     fs.writeFileSync(filename, JSON.stringify(content), 'utf8', (err) => {
@@ -6,8 +8,28 @@ function writeDataToFile(filename, content) {
             console.log(err);
         }
     });
-}
+};
+
+function getPostData(req) {
+    return new Promise((resolve, reject) => {
+        try {
+            let body = '';
+
+            req.on('data', (chunk) => {
+                body += chunk.toString();
+            });
+
+            req.on('end', () => {
+                resolve(body)
+            });
+
+        } catch (error) {
+            reject(error)
+        }
+    });
+};
 
 module.exports = {
     writeDataToFile,
+    getPostData,
 }
